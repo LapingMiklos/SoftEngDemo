b730430b866ae39356da10909cb45afbddeef10c
Laping Miklos-Jozsef <lapingmikus@gmail.com>
2023-12-18 19:48:50 +0200

    added basic project structure

---
+++ HEX/src/main/java/edu/bbte/softeng/lmim2155/hex/Main.java
@@ -0,0 +1,13 @@
+package edu.bbte.softeng.lmim2155.hex;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
